e39fb3e1994941cf7bd44eb7577fc3befcfc86d1
张宝印 <98701392+zhangbaoyin26@users.noreply.github.com>
2022-10-04 21:30:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # 用户数据
 user = {
-	'loginid': '58922',
-	'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzA0ODcwMDM0NTgsInBheWxvYWQiOiJ7XCJpZFwiOjU4OTIyLFwiY29kZVwiOlwiMjAyMTM5MjdcIixcInBhc3N3b3JkXCI6XCJiYTAyMTYwNzM2NTZjMTVlMjIzNTZjNjY4MzA4NjFkOVwiLFwic3R1ZGVudFwiOm51bGx9In0.izyDfsq9msZvNckPvVzubTSvoNaYxvHuQcbSZlDAazs',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53',
+	'loginid': '',
+	'token': '',
+	'User-Agent': '',
 }
 
 # 每次最大请求次数
