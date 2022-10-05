40cd5215c122e9fdf368fa2eceff536f25e27860
zhangbaoyin26 <zhangbaoyin66.gmail.com>
2022-10-05 13:44:32 +0800

    增加了取消预约

--- yushi/config.py
+++ yushi/config.py
@@ -1,11 +1,11 @@
 # 用户数据
 user = {
-	'loginid': '',
-	'token': '',
-	'User-Agent': '',
+	'loginid': '58922',
+	'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzA0ODcwMDM0NTgsInBheWxvYWQiOiJ7XCJpZFwiOjU4OTIyLFwiY29kZVwiOlwiMjAyMTM5MjdcIixcInBhc3N3b3JkXCI6XCJiYTAyMTYwNzM2NTZjMTVlMjIzNTZjNjY4MzA4NjFkOVwiLFwic3R1ZGVudFwiOm51bGx9In0.izyDfsq9msZvNckPvVzubTSvoNaYxvHuQcbSZlDAazs',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53',
 }
 
 # 每次最大请求次数
-max_count = 50
+max_count = 100
 # 每次请求中间间隔+random.uniform(0.5, 1)
 base_time = 0
