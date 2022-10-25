d0d5aca024563354fc81042ec76ea565a8cede0b
zhangbaoyin26 <18275390523@163.com>
2022-10-25 11:57:18 +0800

    format

--- yushi/checkBook.py
+++ yushi/checkBook.py
@@ -16,8 +16,8 @@ def checkBook() -> dict:
 		'Accept': 'application/json, text/javascript, */*; q=0.01',
 		'X-Requested-With': 'XMLHttpRequest',
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52',
-		'loginid': '58922',
-		'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NzQyMDk4ODUyNzMsInBheWxvYWQiOiJ7XCJpZFwiOjU4OTIyLFwiY29kZVwiOlwiMjAyMTM5MjdcIixcInBhc3N3b3JkXCI6XCJiYTAyMTYwNzM2NTZjMTVlMjIzNTZjNjY4MzA4NjFkOVwiLFwic3R1ZGVudFwiOm51bGx9In0.lbRZIiMypvhY2H4oaDfYWQfFgRXU3JPmOV_t9PBPZjE',
+		'loginid': config.user.get('loginid'),
+		'token': config.user.get('token'),
 		'Referer': 'http://ligong.deshineng.com:8082/brmclg/html/main.html?v=3',
 		'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
 		'Accept-Encoding': 'gzip, deflate'
